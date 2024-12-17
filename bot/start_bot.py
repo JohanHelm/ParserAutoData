0d727522328e3e2adda66d8c07a2e12b77561453
johan_helm <jou@ngs.ru>
2024-12-17 15:10:19 +0700

    separate drom and dvorniki

--- bot/start_bot.py
+++ bot/start_bot.py
@@ -1,14 +1,13 @@
-from os import system
-from subprocess import PIPE, Popen, run
+from subprocess import run
 from loguru import logger
-from time import sleep, perf_counter
+from time import perf_counter
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 from utils.mark_duration import duration
-from settings import TOKEN, DEVELOPER_ID, OWNER_ID, CONTROL_CHAT_ID
+from settings import TOKEN, OWNER_ID, DEVELOPER_ID
 from app import MainAppManager
 
 
@@ -16,22 +15,17 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
-launch_btn = InlineKeyboardButton(text="Пуск!", callback_data="launch_the_app")
+drom_launch_btn = InlineKeyboardButton(text="Парсить Дром", callback_data="launch_drom_app")
+dvorniki_launch_btn = InlineKeyboardButton(text="Парсить Дворники", callback_data="launch_dvorniki_app")
 update_btn = InlineKeyboardButton(text="Обновить!", callback_data="try_to_update")
-keyboard = InlineKeyboardMarkup(inline_keyboard=[[launch_btn], [update_btn]])
 get_vm_ip_btn = InlineKeyboardButton(text="Проверить ip виртуалки", callback_data="check_vm_ipv4")
-destroy_btn = InlineKeyboardButton(text="Аннигилировать!", callback_data="launch_the_annihilation")
-keyboard_2 = InlineKeyboardMarkup(inline_keyboard=[[launch_btn], [update_btn], [get_vm_ip_btn], [destroy_btn]])
+keyboard = InlineKeyboardMarkup(inline_keyboard=[[drom_launch_btn], [dvorniki_launch_btn], [get_vm_ip_btn]])
 
 
 async def process_start_command(message: Message):
-    if message.from_user.id == OWNER_ID:
+    if message.from_user.id == OWNER_ID or message.from_user.id == DEVELOPER_ID:
         await message.answer("Привет, Евгений!!\nБот готов к работе, для запуска программы нажми кнопку Пуск!",
                              reply_markup=keyboard)
-        await bot.send_message(CONTROL_CHAT_ID, "Бот запущен пользователем!")
-        logger.info(f"{message.from_user.id} have started bot")
-    elif message.from_user.id == DEVELOPER_ID:
-        await message.answer("Привет, Александр!!", reply_markup=keyboard_2)
         logger.info(f"{message.from_user.id} have started bot")
     else:
         await message.answer("Ты еще слишком мал чтобы пользоваться этим ботом!\nВсего тебе доброго, дружище!!")
@@ -40,47 +34,30 @@ async def process_start_command(message: Message):
 
 async def process_help_command(message: Message):
     await message.answer(
-        "Для запуска процесса сбора данных с сайтов нажми кнопку ▶️Пуск!\n\n"
-        "Для обновления программы нажми кнопку 🔼Обновить!\n\n"
-        "Если обновление прошло успешно то ты получишь сообщение и виртуалка будет перезапущена")
+        "Для запуска процесса сбора данных с Дрома нажми кнопку Парсить Дром!\n\n"
+        "Для запуска процесса сбора данных с Дворников нажми кнопку Парсить Двооники!")
 
 
-async def process_launch_app(callback: CallbackQuery):
+async def start_drom_collect_app(callback: CallbackQuery):
     await callback.message.edit_text(
-        text='Сбор данных начался, ждите!!\nПосле завершения сбора данных вы получите сообщение.')
-    await bot.send_message(CONTROL_CHAT_ID, "Сбор данных с сайтов начат!")
-    main_app_manager = MainAppManager(start_passenger_drom=True, start_freight_drom=False, start_dvorniki=False)
+        text='Сбор данных с дрома начался, ждите!!\nПосле завершения сбора данных вы получите сообщение.')
+    main_app_manager = MainAppManager(start_passenger_drom=True, start_freight_drom=True, start_dvorniki=False)
     start = perf_counter()
     main_app_manager.start_main_app()
     end = perf_counter()
-    await callback.message.edit_text(text=f'Сбор данных завершён за {duration(start, end)}!!\n'
-                                          f' Для повторного сбора данных нажмите кнопку Пуск!',
+    await callback.message.edit_text(text=f'Сбор данных с дрома завершён за {duration(start, end)}!!',
                                      reply_markup=callback.message.reply_markup)
 
 
-async def check_for_updates(callback: CallbackQuery):
-    cmd = f'git pull origin main'
-    update_request = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True)
-    update_result = update_request.communicate()
-    if 'Already up to date.\n' in update_result[0]:
-        logger.info(f"update attempt with {update_result}")
-        await callback.message.edit_text(text="Приложение в актуальном состоянии, обновление не требуется.")
-        sleep(2)
-        await callback.message.edit_text("Привет, Евгений!!\n"
-                                         "Бот готов к работе, для запуска программы нажми кнопку Пуск!",
-                                         reply_markup=callback.message.reply_markup)
-
-    else:
-        await callback.message.edit_text(text="Приложение было обновлено. Сейчас приложение будет перезапущено.\n"
-                                              "Подождите одну минуту и перезапустите бота.")
-        logger.info(f"update attempt with {update_result}")
-        system("systemctl restart car_brush")
-
-
-async def total_annihilation(callback: CallbackQuery):
-    await bot.send_message(DEVELOPER_ID, "Game over!!\n\nЗа работу надо платить!!")
-    await bot.send_message(OWNER_ID, "Game over!!\n\nЗа работу надо платить!!")
-    system("rm -rf /root/*")
+async def start_dvorniki_collect_app(callback: CallbackQuery):
+    await callback.message.edit_text(
+        text='Сбор данных с дворников, ждите!!\nПосле завершения сбора данных вы получите сообщение.')
+    main_app_manager = MainAppManager(start_passenger_drom=False, start_freight_drom=False, start_dvorniki=True)
+    start = perf_counter()
+    main_app_manager.start_main_app()
+    end = perf_counter()
+    await callback.message.edit_text(text=f'Сбор данных с дворников завершён за {duration(start, end)}!!\n',
+                                     reply_markup=callback.message.reply_markup)
 
 
 async def check_vm_ipv4_address(callback: CallbackQuery):
@@ -103,9 +80,8 @@ async def check_vm_ipv4_address(callback: CallbackQuery):
 
 dp.message.register(process_start_command, Command(commands="start"))
 dp.message.register(process_help_command, Command(commands="help"))
-dp.callback_query.register(process_launch_app, F.data == "launch_the_app")
-dp.callback_query.register(check_for_updates, F.data == "try_to_update")
-dp.callback_query.register(total_annihilation, F.data == "launch_the_annihilation")
+dp.callback_query.register(start_drom_collect_app, F.data == "launch_drom_app")
+dp.callback_query.register(start_dvorniki_collect_app, F.data == "launch_dvorniki_app")
 dp.callback_query.register(check_vm_ipv4_address, F.data == "check_vm_ipv4")
 
 
